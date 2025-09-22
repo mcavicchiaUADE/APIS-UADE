42127faf95d7e2ccd998f3a4b949b6cfc832209a
mcavicchia <macavicchia@uade.edu.ar>
2025-09-22 19:19:55 -0300

    Init spring proyect

---
+++ backend/src/test/java/ecomercio/backend/BackGrupoGrupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomercio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackGrupoGrupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
